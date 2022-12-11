e233f6c0361a6487ce3a8c7f86f831ea983b40cf
DrakeOwen <drake@reidfarms.com>
2022-12-11 18:15:59 -0500

    Finshed styling top bar

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,15 @@ import Navbar from './components/Navbar';
 import NewItem from './components/NewItem';
 import EditItem from './components/EditItem';
 import Reporting from './components/Reporting';
+import Search from './components/search';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <div className='top-container'>
+        <div className='top-search'><Search /></div>
+        <div className='top-nav'><Navbar /></div> 
+      </div>
 
       <div className='content-box'>
         <Routes>
---
+++ client/src/components/search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/search.css"
+
+function Search() {
+    return <div>
+        <h1>Search Bar</h1>
+    </div>
+}
+
+export default Search;
